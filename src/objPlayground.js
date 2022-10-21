6421c2bfec658753a4bb80592b327cd6e9620f62
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-10-20 22:45:45 -0300

    added funcition calculator and arrayGenerator

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,30 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  return (
+    {
+      sum: Math.floor(number1 + number2),
+      mult: Math.floor(number1 * number2),
+      div: Math.floor(number1 / number2),
+      sub: Math.floor(number1 - number2)
+    }
+  )
+};
 
-const arrayGenerator = (type, object) => {};
+const arrayGenerator = (type, object) => {
+  let arrayGenerated;
+  if (type === 'keys') {
+    arrayGenerated = Object.keys(object);
+  } else if (type === 'values') {
+    arrayGenerated = Object.values(object);
+  } else if (type === 'entries') {
+    arrayGenerated = Object.entries(object);
+  } else {
+    arrayGenerated = 'type não encontrado escolha keys, values ou entries';
+  }
+  return arrayGenerated;
+};
+console.log(arrayGenerator('entries', calculator(1, 2)));
 
 module.exports = { calculator, arrayGenerator };
